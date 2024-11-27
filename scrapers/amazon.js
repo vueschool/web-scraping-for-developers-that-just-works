fa3ba773dc6e450e1add2ed201c8ae3eb21034a0
Daniel Kelly <me@danielkelly.io>
2024-11-27 10:17:04 -0600

    8-storing-and-managing-scraped-data

--- scrapers/amazon.js
+++ scrapers/amazon.js
@@ -1,10 +1,10 @@
 const pw = require('playwright');
-const AUTH = 'brd-customer-hl_6216694c-zone-scraping_browser1:zhy2f136m7gl';
+const AUTH = 'brd-customer-hl_d6ce8cbe-zone-scraping_browser1:294zjhfdz3ph';
 const SBR_CDP = `wss://${AUTH}@brd.superproxy.io:9222`;
 async function main() {
         console.log('Connecting to Scraping Browser...');
-        const browser = await pw.chromium.connectOverCDP(SBR_CDP);
-        // const browser = await pw.chromium.launch({ headless: false })
+        // const browser = await pw.chromium.connectOverCDP(SBR_CDP);
+        const browser = await pw.chromium.launch({ headless: false })
         try {
                 console.log('Connected! Navigating...');
                 const page = await browser.newPage();
@@ -12,7 +12,13 @@ async function main() {
                 await page.goto(`https://amazon.com/s?k=${encodeURIComponent(booksSearch)}`,{ timeout: 2 * 60 * 1000 });
                 await page.waitForSelector('[data-component-type="s-search-result"]')
 
-                await paginateResults(page, ()=> getBooks(page))
+                let resultsForAllPages = [];
+                await paginateResults(page, async ()=> {
+                        const resultsPerPage = await getBooks(page)
+                        resultsForAllPages = [...resultsForAllPages, ...resultsPerPage]
+                })
+
+                console.log(resultsForAllPages)
 
                 await page.screenshot({ path: './page.png', fullPage: true });
         } finally {
@@ -28,13 +34,25 @@ if (require.main === module) {
 
 async function getBooks(page){
         const books = await page.$$('[data-component-type="s-search-result"]')
+        const results = [];
         for (let i = 0; i < (books.length); i++) {
                 const titleElement = await books[i].$('h2 a span');
-                if (titleElement) {
-                        const title = await titleElement.innerText();
-                        console.log(`${i + 1}. ${title}`);
+                const title = titleElement ? await titleElement.innerText() : '';
+
+                const priceWholeElement = await books[i].$('span.a-price-whole');
+                const priceWhole = !priceWholeElement ? '' : (await priceWholeElement.innerText()).replace('.', '');
+
+                const priceFractionElement = await books[i].$('span.a-price-fraction')
+                const priceFraction = !priceFractionElement ? '' : await (priceFractionElement).innerText();
+
+                const book = {
+                        title,
+                        price: Number(`${priceWhole.trim()}.${priceFraction.trim()}`) || null
                 }
+
+                results.push(book);
         }
+        return results;
 }
 
 async function paginateResults(page, processPage) {
